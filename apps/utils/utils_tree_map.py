5ce0be22d3fcb95762ebe55ccb0f1819a758a3fc
Jose Alejandro Montana <jamontanac@unal.edu.co>
2021-08-24 22:34:52 -0500

    ajustes al tree map, ahora se puede ver toda la jerarquia y adicional se ve hasta orden 3 en profundidad del arbol, de esta forma no se satura al renderizar

--- apps/utils/utils_tree_map.py
+++ apps/utils/utils_tree_map.py
@@ -12,21 +12,28 @@ def Make_tree_map(df):
     new_df=df[columns_in_map].copy()
     new_df['conteo_orden'] = 1
 
-    fig = px.treemap(new_df.dropna(), path=['ORDEN','PAISAJE','CLIMA_AMBIENTAL','FORMA_TERRENO'], values='conteo_orden',
-                  color= 'ORDEN', hover_data=[], color_discrete_map={
+    fig = px.treemap(new_df.dropna(), path=[px.Constant("Total"),'ORDEN','PAISAJE','CLIMA_AMBIENTAL','FORMA_TERRENO'], values='conteo_orden',
+                 color="ORDEN",hover_data=[],maxdepth=3, color_discrete_map={
+                '(?)': '#A0BBD1',
                 "Andisol": '#e74C3C',
                 "Entisol": '#3498DB',
                 "Histosol": '#00BC8C',
                 "Inceptisol": '#375A7F',
                 "Molisol": '#F39C12',
                 })
+    #fig.update_traces(root_color=)
+    #root_color="#9CC1EF"
+    fig.update_traces(name='categories',
+                      root_color='#9CC1EF',
+                      #texttemplate="%{label}<br>%{value}<br>",
+                      selector=dict(type='treemap'))
     fig.update_layout(
         plot_bgcolor="black",
         paper_bgcolor="#222222",
         font_color="#FFFFFF",
         title_font_color="#FFFFFF",
         #font=dict(size=18),
-        margin=dict(l=0, r=0, t=0, b=0)
+        margin = dict(t=50, l=25, r=25, b=25)
         )
 
 
