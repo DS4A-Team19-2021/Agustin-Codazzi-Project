d1b94bceb31c76a505836bef00c0cf221e014e6e
Jose Alejandro Montana <jamontanac@unal.edu.co>
2021-08-27 01:36:41 -0500

    Ajustes para empezar el desarrollo de la API

---
+++ API_Dash_App.py
@@ -0,0 +1,34 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.wsgi import WSGIMiddleware
+from typing import Optional
+from Dash_IGAC.index import create_dash_app
+from pydantic import BaseModel
+import pickle
+import numpy as np
+
+app = FastAPI()
+
+@app.get("/api")
+def read_main():
+    return {
+        "routes": [
+            {"method": "GET", "path": "/api/status", "summary": "App status"},
+            {"method": "POST", "path": "/api/predict", "predict": "Get prediction"},
+        ]
+    }
+
+@app.get("/api/status")
+def get_status():
+    return {"status": "ok"}
+
+dash_app = create_dash_app(routes_pathname_prefix="/")
+app.mount("/", WSGIMiddleware(dash_app.server))
+
+
+if __name__ == "__main__":
+
+    # Run the app with uvicorn ASGI server asyncio frameworks. That basically responds to request on parallel and faster
+
+    uvicorn.run("API_Dash_App:app", host="0.0.0.0", port=8000, reload=True)
+
---
+++ Dash_IGAC/README.md
@@ -0,0 +1,2 @@
+# Agustin-Codazzi-Project
+In this repository there are the codes form the Agustin Codazzi institute in the context of the DS4A course in its version of the 2021.
