83b265fec6635108add608334ce2e82e97d68130
Jose Alejandro Montana <jamontanac@unal.edu.co>
2021-08-27 08:20:32 -0500

    modificacion en el archivo index, parece haber problemas con los callbacks

--- Dash_IGAC/index.py
+++ Dash_IGAC/index.py
@@ -3,7 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 #Best practice libraries
 import dash_auth
-
+import pathlib
 #callbacks import
 from Dash_IGAC.callbacks import register_callbacks
 
@@ -18,6 +18,8 @@ def create_dash_app(routes_pathname_prefix: str = None) -> dash.Dash:
     #Basic auth definition
     USERNAMEINFO = [['user','12345']]
     auth = dash_auth.BasicAuth(app,USERNAMEINFO)
+    # Path
+    BASE_PATH = pathlib.Path(__file__).parent.resolve()
 
     #main layout
     app.layout = html.Div(className='wrapper',
