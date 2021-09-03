7beb4808ab901711ce41da4b7f20abe1fb865380
Jose Alejandro Montana <jamontanac@unal.edu.co>
2021-09-03 00:03:56 -0500

    nuevas modificaciones a los archivos

---
+++ apps/configuration/sqlconnect.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+import json
+x=json.load(open("database_keys.json","r"))
+host = x["host"]
+port = x["port"]
+user = x["user"]
+password = x["password"]
+database = x["database"]
+connDB = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{database}')
+conn = connDB.raw_connection()
+cur = conn.cursor()
